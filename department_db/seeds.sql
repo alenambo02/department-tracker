f3f4090672edd615ca1dc22ca7a37cd0fdb7069e
Ale i <allleizq@gmail.com>
2022-07-26 21:50:14 -0700

    finsihed creating table for employee names and required needed packages

--- department_db/seeds.sql
+++ department_db/seeds.sql
@@ -1,23 +1,20 @@
 INSERT INTO department (department_name)
 VALUES  ("Chemistry"),
-        ("Psychology"),
         ("Engineer"),
         ("Law");
 
 INSERT INTO role_type (title, salary, department_id)
-VALUES  ("Lawyer", 120000.00, 4),
-        ("Legal Assistant", 60000.00, 4),
-        ("Attorney", 80000.00, 4),
-        ("Doctor", 150000.00, 1),
-        ("Nurse", 90000.00, 1),
-        ("ABA Therapist", 50000.00, 2),
-        ("Web Developer", 90000.00, 3),
-        ("Mechanical Engineer", 90000.00, 3);
+VALUES  ("Lawyer", 120000, 3),
+        ("Legal Assistant", 60000, 3),
+        ("Nurse", 90000, 1),
+        ("Web Developer", 90000, 2),
+        ("Mechanical Engineer", 90000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Bobby", "Will", ),
-        ("Pancho", "Villa", ),
-        ("Stephanie", "Rosales", ),
-        ("Liliana", "Rojos", ),
-        ("Eddie", "Perez", );
-
+VALUES  ("Bobby", "Will", 1, null),
+        ("Pancho", "Villa", 3, null),
+        ("Liliana", "Rojos", 4, null),
+        ("Stephanie", "Rosales", 4, 3),
+        ("Rio", "Santo", 5, null),
+        ("Eddie", "Perez", 5, 5),
+        ("Sally", "Ford", 2, null);
