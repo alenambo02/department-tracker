d0cc8bf57a6e59b5cc9effa97aec748681076b74
Ale i <allleizq@gmail.com>
2022-07-27 00:53:35 -0700

    added inital prompt

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+//Connect to mysql database tables
 const employeedb = mysql.createConnection(
     {
       host: 'localhost',
@@ -19,6 +20,21 @@ const employeedb = mysql.createConnection(
     },
     console.log(`Connected to the employeetracker_db database.`)
 );
+
+function displayOptions() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'options',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+        },
+    ])
+    .then((answers) => {
+        console.log(answers)
+    })
+};
 
 
 app.listen(PORT, () => {
