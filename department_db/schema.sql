ae3d6d6bad058c253bc366d63fdc05ebf8bed25e
Ale i <allleizq@gmail.com>
2022-07-26 20:04:15 -0700

    created employee table

--- department_db/schema.sql
+++ department_db/schema.sql
@@ -18,3 +18,13 @@ CREATE TABLE role_type (
   ON DELETE SET NULL
 );
 
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role_type(id)
+  ON DELETE SET NULL
+);
