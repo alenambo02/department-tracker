59b23abf95be207864e755c71eea0c88c14ae312
Ale i <allleizq@gmail.com>
2022-07-27 00:09:30 -0700

    connected server to database, added middleware

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+//Imports express, inquirer, and mysql
 const express = require("express");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+const employeedb = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employeetracker_db'
+    },
+    console.log(`Connected to the employeetracker_db database.`)
+);
 
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
