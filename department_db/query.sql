017869dc75018d7ee898f613ad309cee5d181959
Ale i <allleizq@gmail.com>
2022-07-28 17:51:40 -0700

    created add department function

--- department_db/query.sql
+++ department_db/query.sql
@@ -6,7 +6,7 @@ SELECT role_type.id, role_type.title, department.name, role_type.salary FROM rol
 
 
 
-SELECT employee.id, employee.first_name, employee.last_name, role_type.title, department.name, role_type.salary, employee.manager_id
+SELECT employee.id, employee.first_name AS First name, employee.last_name, role_type.title AS Title, department.name, role_type.salary, employee.manager_id
 FROM employee
 INNER JOIN role_type on employee.role_id = role_type.id
 INNER JOIN department on role_type.department_id = department.id 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Imports inquirer and mysql packages
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cTable = require("console.table");
 
 //so no PORT cause no express???
 // const PORT = process.env.PORT || 3001;
@@ -29,7 +30,7 @@ function displayOptions() {
     inquirer
     .prompt(optionsPrompt)
 
-    .then((userChoice) => {
+    .then(async(userChoice) => {
         switch(userChoice.options) {
             case 'View all departments':
                 employeedb.query(`SELECT * FROM department`, function (err, results) {
@@ -37,7 +38,7 @@ function displayOptions() {
                     console.table(results);
                 
                 })
-                displayOptions()
+               
                 break;
             
             case 'View all roles':
@@ -46,48 +47,88 @@ function displayOptions() {
                     console.table(results);
                 
                 })
-                displayOptions()
+              
                 break;
 
             case 'View all employees':
-                employeedb.query(`SELECT employee.id, employee.first_name, employee.last_name, role_type.title, department.name, role_type.salary, employee.manager_id
+                employeedb.query(`SELECT employee.id, employee.first_name, employee.last_name, role_type.title AS title, department.name, role_type.salary, employee.manager_id AS manager
                 FROM employee
                 INNER JOIN role_type on employee.role_id = role_type.id
-                INNER JOIN department on role_type.department_id = department.id`, function (err, results) {
+                INNER JOIN department on role_type.department_id = department.id `, function (err, results) {
                     console.log('hello')
                     console.table(results);
                 })
-                displayOptions()
+               
                 break;
 
             case 'Add a department':
                
-
-                displayOptions()
+                await addDepartment()
+               
                 break;
 
             case 'Add a role':
                
-
-                displayOptions()
+              
                 break;
 
             case 'Add an employee':
                 
-                displayOptions()
+              
                 break;
 
             case 'Add an employeeUpdate an employee role':
                 
-                displayOptions()
+             
                 break;
 
         }
+        displayOptions()
     
     })
 }
 
 displayOptions()
+
+
+
+
+
+
+function addDepartment() {
+    return new Promise(resolve => {
+        inquirer
+        .prompt({
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'departmentName',
+        })
+        .then((answer) => {
+        employeedb.query(`INSERT INTO department (name) VALUES (?)`, [answer.departmentName], 
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Department was added!")
+            resolve("resolve")
+        }) 
+        })
+    }) 
+}
+
+// function addRole() {
+
+// }
+
+// function addEmployee() {
+
+// }
+
+
+
+
+
+
 
 
 
